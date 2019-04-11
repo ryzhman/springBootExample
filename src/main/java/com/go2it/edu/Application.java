857474c56e3e495ec12f6bff76b348d2a49c3088
Oleksandr Ryzhkov <ryzhman@gmail.com>
2019-04-10 23:16:39 -0400

    Basic Spring Boot configuration + .gitignore file

---
+++ src/main/java/com/go2it/edu/Application.java
@@ -0,0 +1,15 @@
+package com.go2it.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alex Ryzhkov
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
