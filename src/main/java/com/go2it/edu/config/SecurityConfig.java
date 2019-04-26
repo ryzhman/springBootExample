64589d2f063a04a1c178e680c3a4feabeef8a066
Oleksandr Ryzhkov <ryzhman@gmail.com>
2019-04-26 00:17:40 -0400

    Basic Spring Security implementation
    fix

---
+++ src/main/java/com/go2it/edu/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.go2it.edu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Alex Ryzhkov
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	public SecurityConfig() {
+		super();
+		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+	}
+
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password(encoder().encode("adminPass"))
+				.roles("ADMIN")
+				.and()
+				.withUser("user")
+				.password(encoder().encode("userPass"))
+				.roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+				.and().csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/api/welcome")
+				.permitAll()
+				//				even anonymous user is authenthicated
+				//				.authenticated()
+				//				 .hasAnyRole()
+				.antMatchers(HttpMethod.POST, "/api/customizedWelcome")
+				.permitAll()
+				.antMatchers("/api/user/**")
+				.hasAnyRole("ADMIN", "USER")
+				.antMatchers("/api/resource/**")
+				.hasRole("ADMIN")
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin()
+				.permitAll();
+	}
+}
