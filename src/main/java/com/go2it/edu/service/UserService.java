231c2a838f268856063bfdc90663e3825a1f3479
Oleksandr Ryzhkov <ryzhman@gmail.com>
2020-07-31 07:29:16 -0400

    When the user is created via API, password is encoded and safely stored in DB

--- src/main/java/com/go2it/edu/service/UserService.java
+++ src/main/java/com/go2it/edu/service/UserService.java
@@ -3,6 +3,7 @@ package com.go2it.edu.service;
 import com.go2it.edu.entity.User;
 import com.go2it.edu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,13 @@ import java.util.List;
 public class UserService implements IUserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void save(User newUser) {
+        String encodedPassword = passwordEncoder.encode(newUser.getPassword());
+        newUser.setPassword(encodedPassword);
         userRepository.save(newUser);
     }
 
