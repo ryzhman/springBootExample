a22d67556e4ccf7bb64ebb8ac1db45fd312d86c2
Oleksandr Ryzhkov <ryzhman@gmail.com>
2020-07-31 07:50:32 -0400

    Custom UserPrincipal gets the list of roles the corresponding user is granted

--- src/main/java/com/go2it/edu/entity/security/UserPrincipal.java
+++ src/main/java/com/go2it/edu/entity/security/UserPrincipal.java
@@ -2,10 +2,15 @@ package com.go2it.edu.entity.security;
 
 import com.go2it.edu.entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
+/**
+ * Object to represent the authenticated user with all the required User details and granted permissions
+ */
 public class UserPrincipal implements UserDetails {
     private User user;
 
@@ -13,10 +18,15 @@ public class UserPrincipal implements UserDetails {
         this.user = user;
     }
 
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        //convert the list of String roles into a proper Spring Security objects
+        return user.getRoles()
+                .stream()
+                //Spring Security requires all ROLES added in the memory to be prefixed with ROLE_
+                //we don't store tole name with this prefix in DB
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
     }
 
     @Override
