81395ecb2b24261e942e6b75e7c8203b1088a663
Oleksandr Ryzhkov <ryzhman@gmail.com>
2020-08-01 07:18:30 -0400

    Per-session CSRF token is passed back to the client in Cookies so it can be re-used for further requests

--- src/main/java/com/go2it/edu/config/SecurityConfig.java
+++ src/main/java/com/go2it/edu/config/SecurityConfig.java
@@ -14,7 +14,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.SecureRandom;
 
 /**
@@ -60,8 +72,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
-                //By default is enabled for POST requests
-                .csrf().disable()
+                //By default is enabled for any modifying requests (POST, PUT etc)
+//                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/api/welcome")
                     .permitAll()
@@ -78,6 +90,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .hasAnyRole("ADMIN")
                 .and()
                     .formLogin()
-                    .permitAll();
+                    .permitAll()
+                .and()
+                    .csrf().csrfTokenRepository(csrfTokenRepository())
+                //Add the CSRF token explicitly to the request cookies to be able to re-use it
+                //on per-session case
+                .and()
+                    .addFilterAfter(new OncePerRequestFilter() {
+                    @Override
+                    protected void doFilterInternal(HttpServletRequest request,
+                                                    HttpServletResponse response, FilterChain filterChain)
+                            throws IOException, ServletException {
+                        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                                .getName());
+                        if (csrf != null) {
+                            Cookie cookie = WebUtils.getCookie(request, "X-CSRF-TOKEN");
+                            String token = csrf.getToken();
+                            if (cookie == null || token != null
+                                    && !token.equals(cookie.getValue())) {
+                                cookie = new Cookie("X-CSRF-TOKEN", token);
+                                cookie.setPath("/");
+                                response.addCookie(cookie);
+                            }
+                        }
+                        filterChain.doFilter(request, response);
+                    }
+                    }, CsrfFilter.class);
+    }
+
+    /**
+     * Add CSRF token to each response as a header
+     * @return
+     */
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-CSRF-TOKEN");
+        return repository;
     }
 }
